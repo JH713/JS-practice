48624fb9a3d30242df2f6b414d489e35a0e42341
이지현 <strider13@snu.ac.kr>
2023-07-30 00:11:01 +0900

    배경 및 greeting 페이지 css 수정

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -2,8 +2,12 @@ const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+// const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+const background = document.querySelector("#background");
 
-document.body.appendChild(bgImage);
+// bgImage.src = `img/${chosenImage}`;
+
+// document.body.appendChild(bgImage);
+
+background.style.backgroundImage=`linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(img/${chosenImage})`;
--- momentum/js/greetings.js
+++ momentum/js/greetings.js
@@ -1,6 +1,8 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const firstPage = document.querySelector("#first-page");
+const mainPage = document.querySelector("#main-page");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -8,13 +10,14 @@ const USERNAME_KEY = "username";
 function paintGreetings()
 {
 	const username = localStorage.getItem(USERNAME_KEY);
-	greeting.innerText = `Hello ${username}`;
-	greeting.classList.remove(HIDDEN_CLASSNAME);
+	greeting.innerText = `Hi, ${username}`;
+	mainPage.classList.remove(HIDDEN_CLASSNAME);
+	firstPage.classList.add(HIDDEN_CLASSNAME);
 }
 
 function onLoginSubmit(event) {
 	event.preventDefault();
-	loginForm.classList.add(HIDDEN_CLASSNAME);
+	firstPage.classList.add(HIDDEN_CLASSNAME);
 	const username = loginInput.value;
 	localStorage.setItem(USERNAME_KEY, username);
 	paintGreetings();
@@ -25,6 +28,6 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 if (savedUsername) {
 	paintGreetings();
 } else {
-	loginForm.classList.remove(HIDDEN_CLASSNAME);
+	firstPage.classList.remove(HIDDEN_CLASSNAME);
 	loginForm.addEventListener("submit", onLoginSubmit);
 }
