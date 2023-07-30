de0cddeb7e02dc464885c456c08e2781419a00e0
이지현 <strider13@snu.ac.kr>
2023-07-30 22:54:25 +0900

    로그아웃 구현 및 메인페이지 css 완성

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -4,8 +4,8 @@ function displayTime() {
 	const date = new Date();
 	const hour = String(date.getHours()).padStart(2, "0");
 	const min = String(date.getMinutes()).padStart(2, "0");
-	const sec = String(date.getSeconds()).padStart(2, "0");
-	clock.innerText = `${hour}:${min}:${sec}`;
+	// const sec = String(date.getSeconds()).padStart(2, "0");
+	clock.innerText = `${hour}:${min}`;
 }
 
 displayTime();
--- momentum/js/greetings.js
+++ momentum/js/greetings.js
@@ -3,6 +3,7 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const firstPage = document.querySelector("#first-page");
 const mainPage = document.querySelector("#main-page");
+const logout = document.getElementById("logout");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -13,6 +14,7 @@ function paintGreetings()
 	greeting.innerText = `Hi, ${username}`;
 	mainPage.classList.remove(HIDDEN_CLASSNAME);
 	firstPage.classList.add(HIDDEN_CLASSNAME);
+	logout.addEventListener("click", onLogoutClick);
 }
 
 function onLoginSubmit(event) {
@@ -21,6 +23,12 @@ function onLoginSubmit(event) {
 	const username = loginInput.value;
 	localStorage.setItem(USERNAME_KEY, username);
 	paintGreetings();
+}
+
+function onLogoutClick(event) {
+	localStorage.removeItem(USERNAME_KEY);
+	localStorage.removeItem("todos");
+	window.location.reload();
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
